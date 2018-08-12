e033c3e7462e1e1cf5c1c5ff0c57cfb2d75ba11a
Genokiller <Genokiller@qq.com>
2018-08-13 00:15:45 +0800

    添加了备胎岛domain，默认为备胎岛

--- app/src/main/java/com/hippo/nimingban/client/ac/ACUrl.java
+++ app/src/main/java/com/hippo/nimingban/client/ac/ACUrl.java
@@ -23,9 +23,9 @@ public class ACUrl {
 
     public static final String APP_ID = "nimingban";
 
-    public static final String DOMAIN = "adnmb1.com";
+    public static final String DOMAIN = "tnmb.org";
 
-    public static final String HOST = "http://" + DOMAIN;
+    public static final String HOST = "https://" + DOMAIN;
 
     public static final String API_POST_LIST = HOST + "/Api/showf?appid=" + APP_ID;
 
